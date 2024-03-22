ee13a2f3203f32aa226a3146f39a5048bfcf027e
Ahmad <ahmad.kaleem@eurustechnologies.com>
2024-03-22 20:57:39 +0500

    dummy commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-to-store-remote-state-file"
+    bucket         = "ahmad-terraform-backend-to-store-remote-state-file"
     key            = "terraform.tfstate"
     # dynamodb_table = "terraform-lock"
     region         = "us-west-1"
